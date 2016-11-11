771a3dbb0fcf5766f10153c8e8dfb36f2326bfaf
TheNamlessGuy <bragux@hotmail.com>
2016-11-11 21:33:34 +0100

    Stuff

--- main.rb
+++ main.rb
@@ -2,17 +2,20 @@ require 'gosu'
 require './player.rb'
 
 class GameWindow < Gosu::Window
-  p = Player.new
-
   def initialize
     super 640, 480
-    self.caption = "TEST"
+    self.caption = ""
+    
+    @player = Player.new(100, 100)
   end
 
   def update
+    @player.update
   end
 
   def draw
+    # Draw map, then player, then hud
+    @player.draw
   end
 end
 
--- player.rb
+++ player.rb
@@ -1,5 +1,32 @@
 class Player
-  def initialize
-    
+  def initialize(x, y)
+    @x = x
+    @y = y
+  end
+
+  def update
+    if Gosu::button_down? Gosu::KbRight then
+      # Move right
+      puts "Move Right"
+    elsif Gosu::button_down? Gosu::KbLeft then
+      # Move left
+      puts "Move left"
+    elsif Gosu::button_down? Gosu::KbDown then
+      # Aim down
+      puts "Aim down"
+    elsif Gosu::button_down? Gosu::KbUp then
+      # Aim up
+      puts "Aim up"
+    elsif Gosu::button_down? Gosu::KbX then
+      # Shoot
+      puts "Shoot"
+    elsif Gosu::button_down? Gosu::KbZ then
+      # Jump
+      puts "Jump"
+    end
+  end
+
+  def draw
+
   end
 end
