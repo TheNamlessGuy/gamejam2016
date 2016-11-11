3eaeaef75a84b9e2d9542442ba66d0405c7cf047
Henrik Ingbrant Björs <henbj316@student.liu.se>
2016-11-11 22:14:38 +0100

    Split map into two files

--- map.rb
+++ map.rb
@@ -1,36 +1,14 @@
 require "gosu"
 
-class MapHitBox
-  def initialize (x, y, w, h)
-    @box = [x, y, w, h]
-    @type = :SOLID
-  end
-
-  def gethitbox
-    return @box
-  end
-
-  def gettype
-    return @type
-  end
-end
-
-class MapGround
-  def initialize (x1, x2, type)
-    @ground = [x1, 500, x2 - x1, 600]
-    @type = type
-  end
-
-  def gethitbox
-    return @ground
-  end
-
-  def gettype
-    return @type
-  end
-end
+require "./map_helpers.rb"
 
 class Map
+
+  #PUBLIC FUNCTIONS
+  #bool loadmap(mapname)
+  #collisiontype collisioncheck(collisionbox)
+  #void draw(camx, camy)
+
   def initialize
     @map = []
   end
