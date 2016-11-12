ca0935a352b06c5aa708f01e7f1ac7a1b52b8a52
Henrik Ingbrant Björs <henbj316@student.liu.se>
2016-11-12 19:31:47 +0100

    Coins falling now, yes

--- coin.rb
+++ coin.rb
@@ -4,13 +4,20 @@ class Coin
   attr_accessor :hitbox, :value
 
   def initialize(x, y)
+    @yVel = 0
+    
     @hitbox = HitBox.new(x, y, 25, 25)
     @@image = Gosu::Image.new("res/coins.png")
     @value = 10
   end
 
   def update(delta)
-    
+    @yVel += 0.1
+    @hitbox.get[1] += @yVel * delta
+    if @hitbox.get[1]+@hitbox.get[3] >= 500
+      @yVel = 0
+      @hitbox.get[1] = 500 - @hitbox.get[3]
+    end
   end
 
   def draw(camX)
