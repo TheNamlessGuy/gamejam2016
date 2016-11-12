03ecce04389378f25f665365f3f3594cefa16d80
Markus Lewin <marle943@student.liu.se>
2016-11-12 17:15:09 +0100

    cutscene:)

---
+++ cutscene.rb
@@ -0,0 +1,22 @@
+class Cutscene
+  def initialize(scenes)
+    @current_scene = 0
+    @scenes = scenes
+  end
+
+  def update(delta_time)
+    event = @scenes[@current_scene].update delta_time
+
+    if event == :scene_end
+      if @current_scene < @scenes.length - 1
+        @current_scene += 1
+      else
+        return :cutscene_end
+      end
+    end
+  end
+
+  def draw
+    @scenes[@current_scene].draw
+  end
+end
