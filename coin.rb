fda9210046277d04f4757b4b79618a516fb5e77d
TheNamlessGuy <bragux@hotmail.com>
2016-11-13 12:04:34 +0100

    particles spawn free LOOTZ yo brush

--- coin.rb
+++ coin.rb
@@ -1,26 +1,22 @@
 require './hitbox.rb'
 
 class Coin
-  attr_accessor :hitbox, :value
+  attr_accessor :hitbox, :value, :yVel
 
-  def initialize(x, y)
-    @yVel = 0
+  def initialize(x, y, type)
+    @yVel = -0.5
     
-    @hitbox = HitBox.new(x, y, 25, 25)
-    @@image = Gosu::Image.new("res/coins.png")
-    @value = 10
+    @hitbox = HitBox.new(x, y, 16, 16)#25, 25)
+    @image = Gosu::Image.new("res/" + type + ".png")
+    @value = (type == "bill" ? 20 : 10)
   end
 
   def update(delta)
-    @yVel += 0.1
+    @yVel += 0.05
     @hitbox.get[1] += @yVel * delta
-    if @hitbox.get[1]+@hitbox.get[3] >= 500
-      @yVel = 0
-      @hitbox.get[1] = 500 - @hitbox.get[3]
-    end
   end
 
   def draw(camX)
-    @@image.draw(@hitbox.get[0] - camX, @hitbox.get[1], 1)
+    @image.draw(@hitbox.get[0] - camX, @hitbox.get[1], 1)
   end
 end
